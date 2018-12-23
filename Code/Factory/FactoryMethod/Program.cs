ae9329a2e7d64cd124b6aebd304108a802a55c2d
Chris Klug <chris@59north.com>
2018-12-23 21:12:44 +0100

    Added factory method

---
+++ Code/Factory/FactoryMethod/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateHelper dateHelper;
+#if DEBUG
+            dateHelper = new DateHelper();
+#else
+            dateHelper = new FancyDateHelper();
+#endif
+
+            dateHelper.IsItFriday();
+
+            Console.ReadKey();
+        }
+    }
+
+    interface ILogger
+    {
+        void Log(string message);
+    }
+
+    class ConsoleLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+    class FancyLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            var color = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine(message);
+            Console.ForegroundColor = color;
+        }
+    }
+
+    class DateHelper {
+        public bool IsItFriday()
+        {
+            var day = DateTime.Now.DayOfWeek;
+            GetLogger().Log($"Is { day.ToString() } friday? { day == DayOfWeek.Friday }");
+            return day == DayOfWeek.Friday;
+        }
+
+        protected virtual ILogger GetLogger()
+        {
+            return new ConsoleLogger();
+        }
+    }
+    class FancyDateHelper : DateHelper
+    {
+        protected override ILogger GetLogger()
+        {
+            return new FancyLogger();
+        }
+    }
+}

7958bd58a7dae1beab11933fa4ab3c954ac4782e
Chris Klug <chris@59north.com>
2018-12-24 14:22:35 +0100

    Added static factory method example

---
+++ Code/Factory/StaticFactoryMethod/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace StaticFactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dateHelper = DateHelper.Create(false);
+            var isItFriday = dateHelper.IsItFriday();
+            
+            dateHelper = DateHelper.Create(true);
+            isItFriday = dateHelper.IsItFriday();
+
+            Console.ReadKey();
+        }
+    }
+
+    class DateHelper
+    {
+        protected DateHelper()
+        {
+
+        }
+
+        public static DateHelper Create(bool addLogging = false)
+        {
+            return addLogging ? new DateHelper() : new NonLoggingDateHelper();
+        }
+
+        public bool IsItFriday()
+        {
+            var day = DateTime.Now.DayOfWeek;
+            Log($"Is { day.ToString() } friday? { day == DayOfWeek.Friday }");
+            return day == DayOfWeek.Friday;
+        }
+
+        protected virtual void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+    class NonLoggingDateHelper : DateHelper
+    {
+        protected override void Log(string message) {}
+    }
+}

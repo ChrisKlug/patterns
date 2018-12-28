4b2b72985cddf7efad443243beec6ad38f9f5216
Chris Klug <chris@59north.com>
2018-12-28 10:27:55 +0100

    Added proxy demo

---
+++ Code/Adapters/Proxy/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Security.Authentication;
+using System.Threading;
+
+namespace Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IIsItFridayService svc = new IsItFridayProxy();
+
+            Console.WriteLine("Is it friday? {0}", svc.IsItFriday());
+
+            Console.ReadKey();
+        }
+    }
+
+    interface IIsItFridayService
+    {
+        bool IsItFriday();
+    }
+
+    class IsItFridayProxy : IIsItFridayService
+    {
+        IIsItFridayService _instance;
+
+        public bool IsItFriday()
+        {
+            if (!Environment.UserName.Equals("Chris", StringComparison.OrdinalIgnoreCase))
+            {
+                throw new AuthenticationException("You are not allowed to do this");
+            }
+            return Instance.IsItFriday();
+        }
+
+        private IIsItFridayService Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new IsItFridayService();
+                }
+                return _instance;
+            }
+        }
+    }
+
+    class IsItFridayService : IIsItFridayService
+    {
+        public bool IsItFriday()
+        {
+            return DateTime.UtcNow.DayOfWeek == DayOfWeek.Friday;
+        }
+    }
+}

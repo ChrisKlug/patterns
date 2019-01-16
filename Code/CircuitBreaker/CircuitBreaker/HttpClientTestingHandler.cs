41a9af26f801daad812a231c2b953b98ec19559a
Chris Klug <chris@59north.com>
2019-01-16 14:37:08 +0100

    Fixed spelling of circuit breaker

--- Code/CircuitBreaker/CircuitBreaker/HttpClientTestingHandler.cs
+++ Code/CircuitBreaker/CircuitBreaker/HttpClientTestingHandler.cs
@@ -4,7 +4,7 @@ using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace CricuitBreaker
+namespace CircuitBreaker
 {
     class HttpClientTestingHandler : HttpMessageHandler
     {
--- Code/CircuitBreaker/CircuitBreaker/RequestHelper.cs
+++ Code/CircuitBreaker/CircuitBreaker/RequestHelper.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Net.Http;
 
-namespace CricuitBreaker
+namespace CircuitBreaker
 {
     static class RequestHelper
     {
